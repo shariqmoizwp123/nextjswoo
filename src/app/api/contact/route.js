e599d0dd40428660b1613fcb7c205d1df8e47a65
sharoqmoizwp123 <wpustaadpk@gmail.com>
2026-05-25 08:32:28 +0500

    contactusapiemail

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_iiBmtiyt_LBds1U7xbTx7A9vXzSvJstNR);
 
 export async function POST(req) {
   try {
